e8837095f68e0fdd79f56f722c6c1429cba87871
FelipeTRpo <felipetrpoke@hotmail.com>
2021-11-21 22:21:19 -0300

    modulo03parte02js terminado

--- Modulo03parte02js/usando-querry-selector.js
+++ Modulo03parte02js/usando-querry-selector.js
@@ -1,9 +1,27 @@
-
-
-
 function modColor(idTag,newColor){
     let element = document.querySelector(idTag);
     element.style.color = newColor;
 }
+function modFontSize(idTag,newSize){
+    let element = document.querySelector(idTag);
+    element.style.fontSize = newSize;
+}
+function modLineHeight(idTag,newLineHeight){
+    let element = document.querySelector(idTag);
+    element.style.lineHeight = newLineHeight;
+}
+function modPaddingTop(idTag,newPaddingTop){
+    let element = document.querySelector(idTag);
+    element.style.paddingTop = newPaddingTop;
+}
 
-modColor('#nome', 'blue')
+modFontSize('#nome','50px');
+modColor('#nome', '#00ADB5');
+modFontSize('#nome-banda-favorita','50px');
+modLineHeight('#nome-banda-favorita','2.5em');
+modFontSize('.texto-musicas-favoritas','30px')
+modPaddingTop('#texto-banda-favorita', '30px')
+modLineHeight('.texto-musicas-favoritas','2.5em');
+modLineHeight('#lista-de-musicas','1.5em');
+modLineHeight('#texto-banda-favorita','1.5em');
+modLineHeight('#link-para-consulta','1.5em');
