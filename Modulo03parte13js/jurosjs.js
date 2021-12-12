f1a76ca07a9d4a8a0affab95577d23416c3ce920
FelipeTRpo <felipetrpoke@hotmail.com>
2021-12-12 14:15:22 -0300

    juros mod

--- Modulo03parte13js/jurosjs.js
+++ Modulo03parte13js/jurosjs.js
@@ -1,5 +1,7 @@
 let contasClientes = [];
 document.getElementById("valores-cadastrados").style.display = "none"
+let todayDate = Date();
+const oneDay = 1000 * 60 * 60 * 24;
 function cadastroCliente(){    
     let theBill = {
         clientName : document.getElementById('nome-cliente').value,
@@ -19,9 +21,9 @@ function cadastroCliente(){
         document.getElementById('digite-todos').innerHTML="Digite todos os campos para completar o registro";
     }else{
         contasClientes.push(theBill);
-        document.getElementById('digite-todos').innerHTML= "";
+        document.getElementById('digite-todos').innerHTML= "Compra cadastrada com sucesso";
+
     }
-    console.log(contasClientes);
 }
 function irCadastro(){
     document.getElementById("cadastro-conta").style.display = "none"
@@ -33,5 +35,44 @@ function voltarCadastro(){
 }
 
 function calcularJuros(){
+    document.getElementById('cadastros').innerHTML= "";
+    let cadastrosHeader = {
+        clientName : "Nome",
+        expireDate : "Data de vencimento",
+        buyAmount : "Valor a ser pago"
+    }
+    showAll(cadastrosHeader)
+    function aplicarJuros(obj){
 
+        let today = new Date(todayDate);
+        let expire = new Date(obj.expireDate)
+        let dateComparison = ((today.getTime()-expire.getTime()) /(oneDay)).toFixed(0);
+        obj.buyAmount = parseInt(obj.buyAmount)
+        let objMod = {
+            clientName : obj.clientName,
+            expireDate : obj.expireDate,
+            buyAmount : obj.buyAmount
+        }
+        if(dateComparison>0){
+            objMod.buyAmount += objMod.buyAmount*((dateComparison*0.001)+0.02);
+        }
+        objMod.expireDate = objMod.expireDate.split('-').reverse().join('/');
+        return objMod;
+    }
+    let cadastrosComJuros = contasClientes.map(aplicarJuros)
+function showAll(obj2){
+    let coluna = document.createElement('div')
+    coluna.classList.add('coluna-cadastro');
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    let span3 = document.createElement('span')
+    span1.innerHTML = obj2.clientName+" ";
+    span2.innerHTML = obj2.expireDate;
+    span3.innerHTML = " R$"+obj2.buyAmount+" ";
+    coluna.appendChild(span1);
+    coluna.appendChild(span2);
+    coluna.appendChild(span3);
+    document.getElementById('cadastros').appendChild(coluna);
+}
+    cadastrosComJuros.forEach(showAll)
 }
