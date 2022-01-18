51e3ffe93416f2ebcd5a0d3f32e24455db29a2a9
FelipeTRpo <felipetrpoke@hotmail.com>
2022-01-18 11:04:59 -0300

    rest add

---
+++ Modulo03parte20js/restoperator.js
@@ -0,0 +1,15 @@
+function callMe(...infinitearray){
+    return thearray = infinitearray;
+}
+const a = 10;
+const b = 92;
+const c = 419;
+const d = 50;
+const e = 68;
+const f = 15;
+const g = 52;
+const h = 57;
+
+const allInOne = callMe(a,b,c,d,e,f,g,h);
+
+console.log(allInOne);
