1cf275478542284ec66f279b7c8dfde275149353
FelipeTRpo <felipetrpoke@hotmail.com>
2022-02-11 12:47:38 -0300

    add mapa1

--- Modulo04parte03jquerry/mapa.js
+++ Modulo04parte03jquerry/mapa.js
@@ -1,9 +1,11 @@
 $("#btn").on("click",function(){
-    let cepDigitado = $("#cep").val().split("-").join("")
-    console.log(cepDigitado)
-    $.ajax({url:`https://cep.awesomeapi.com.br/json/${cepDigitado}`}).done((data)=>{
-        console.log(data)
-        $("#endereco").html(`<p>Endereço: <br /> Rua: ${data.address}<br />Bairro: ${data.district}<br />Cidade: ${data.city}<br />Estado: ${data.state}<p/>`)
-        $("#map").html(`<iframe src="https://www.google.com/maps?api=1&q=${data.lat}%2C${data.lng}&hl=es;z=14&output=embed" width="600" height="450" style="border:0;" ></iframe>`)
-    });
+    if($("#cep").val().split("").length === 9 && $("#cep").val().split("")[5] ==="-" ){
+        let cepDigitado = $("#cep").val().split("-").join("")
+        $.ajax({url:`https://cep.awesomeapi.com.br/json/${cepDigitado}`}).done((data)=>{
+            $("#endereco").html(`<p>Endereço: <br /> Rua: ${data.address}<br />Bairro: ${data.district}<br />Cidade: ${data.city}<br />Estado: ${data.state}<p/>`)
+            $("#map").html(`<iframe src="https://www.google.com/maps?api=1&q=${data.lat}%2C${data.lng}&hl=es;z=14&output=embed" width="600" height="450" style="border:0;" ></iframe>`)
+        });
+}else{
+    $("#endereco").html("Digite um CEP valido")
+}
 });
